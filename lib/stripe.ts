e8c600a2211be5428f1babc237877fa1eeac8a3a
RejectModders <liamkael2007@gmail.com>
2026-03-13 00:45:57 -0500

    chore: refactor Stripe initialization to lazy-load instance and handle missing secret key

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,20 @@
 import 'server-only'
-
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+let stripeInstance: Stripe | null = null
+
+export const stripe = new Proxy({} as Stripe, {
+    get(_, prop) {
+        if (!stripeInstance) {
+            const key = process.env.STRIPE_SECRET_KEY
+
+            if (!key) {
+                throw new Error("STRIPE_SECRET_KEY is not set")
+            }
+
+            stripeInstance = new Stripe(key)
+        }
+
+        return stripeInstance[prop as keyof Stripe]
+    },
+})
